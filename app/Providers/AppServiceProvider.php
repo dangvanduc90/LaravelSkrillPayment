c5cd81805d42f83233e426b4085f825cf8b7eed3
dangvanduc90 <dangvanduc0@gmail.com>
2020-01-13 17:03:45 +0700

    auth + artisan eloquent search

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Articles\ArticlesRepository;
+use App\Articles\EloquentRepository;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +15,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind(
+            ArticlesRepository::class,
+            EloquentRepository::class
+        );
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,106 @@
 |
 */
 
+use App\Article;
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
+use App\Articles\ArticlesRepository;
+
 Route::get('/', function () {
+    $logger = new Logger('elasticquent_log');
+    $logger->pushHandler(new StreamHandler(storage_path('logs/elasticquent_log.log'), Logger::WARNING));
+    $client = Elasticsearch\ClientBuilder::create()
+    ->setLogger($logger)
+    ->build();
+
+    // #_index_a_document
+    // $params = [
+    //     'type' => config('elasticquent.default_index'),
+    //     'index' => 'my_index',
+    //     'id'    => 'my_id',
+    //     'body'  => ['testField' => 'abc']
+    // ];
+    // $response = $client->index($params);
+    // dd($response);
+
+    # _get_a_document
+    $params = [
+        'type' => config('elasticquent.default_index'),
+        'index' => 'my_index',
+        'id'    => 'my_id11',
+        'client' => [
+            'ignore' => [404],
+            'timeout' => 10,        // ten second timeout
+            // 'verbose' => true,
+        ],
+    ];
+    $response = $client->get($params);
+    dd($response);
+
+    // #_search_for_a_document
+    // $params = [
+    //     'index' => 'my_index',
+    //     'body'  => [
+    //         'query' => [
+    //             'match' => [
+    //                 'testField' => 'abc'
+    //             ]
+    //         ]
+    //     ]
+    // ];
+    // $response = $client->search($params);
+    // dd($response);
+
+    // #_delete_a_document
+    // $params = [
+    //     'type' => config('elasticquent.default_index'),
+    //     'index' => 'my_index',
+    //     'id'    => 'my_id'
+    // ];
+    // $response = $client->delete($params);
+    // dd($response);
+
+    // #_delete_an_index
+    // $deleteParams = [
+    //     'index' => 'my_index'
+    // ];
+    // $response = $client->indices()->delete($deleteParams);
+    // dd($response);
+
+    // #_create_an_index
+    // $params = [
+    //     'index' => 'my_index',
+    //     'body' => [
+    //         'settings' => [
+    //             'number_of_shards' => 2,
+    //             'number_of_replicas' => 0
+    //         ]
+    //     ]
+    // ];
+    // $response = $client->indices()->create($params);
+    // dd($response);
     return view('welcome');
 });
+
+Route::get('/search', function() {
+
+    $articles = Article::searchByQuery(['match' => ['title' => 'Sed']]);
+
+    return $articles;
+});
+
+Route::get('/search', function (ArticlesRepository $repository) {
+    $articles = $repository->search((string) request('q'));
+
+    return view('articles.index', [
+        'articles' => $articles,
+    ]);
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
